49629bfc074af53f2c97e62ee8de7e40407d0a1d
Deniz BAYAT <denizbyat@gmail.com>
2026-02-17 12:47:06 +0300

    Docker Kuruldu ve Entegre Edildi

--- app.py
+++ app.py
@@ -157,5 +157,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    print("🚀 Sunucu başlatılıyor... http://127.0.0.1:5000")
-    app.run(debug=True, port=5000)
+    # host='0.0.0.0' ekledik. Bu, dışarıdan erişimi açar.
+    print("🚀 Sunucu başlatılıyor... http://0.0.0.0:5000")
+    app.run(debug=True, host='0.0.0.0', port=5000)
